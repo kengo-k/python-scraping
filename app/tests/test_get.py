c06fcd223bda0faca1d61cf6ae1b1038544cb382
kengo-k <kurobanekengo@gmail.com>
2022-09-18 21:02:24 +0900

    webserverを起動して実際にスクレイピングするテストを追加

---
+++ app/tests/test_get.py
@@ -0,0 +1,40 @@
+import unittest
+import threading
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from playwright.sync_api import sync_playwright
+
+from lib.arg import get_parser
+from lib.get import exec as get
+
+HOST = "localhost"
+PORT = 3000
+URL = f"http://{HOST}:{PORT}/test.html"
+print(URL)
+
+class Server(HTTPServer):
+  def run(self):
+    try:
+      self.serve_forever()
+    except KeyboardInterrupt:
+      pass
+    finally:
+      self.server_close()
+
+class TestGet(unittest.TestCase):
+
+  def setUp(self):
+    self.server = Server((HOST, PORT), SimpleHTTPRequestHandler)
+    self.thread = threading.Thread(None, self.server.run)
+    self.thread.start()
+    self.parser = get_parser()
+
+  def tearDown(self):
+    self.server.shutdown()
+    self.thread.join()
+
+  def test_attribute(self):
+    with sync_playwright() as playwright:
+      args = self.parser.parse_args([URL,  "a[id='link1']", '-a', 'href'])
+      result = get(playwright, args)
+      self.assertEqual(result, "/foo.html")
+
