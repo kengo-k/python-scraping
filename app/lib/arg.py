216bb22ce6693f62c9380886e67aa746a47beb96
kengo-k <kurobanekengo@gmail.com>
2022-09-18 21:40:13 +0900

    コマンドラインオプションの関連バリデーションを追加

--- app/lib/arg.py
+++ app/lib/arg.py
@@ -1,9 +1,22 @@
 import argparse
+from enum import Enum, auto
+
+class ResultCode(Enum):
+  E0001 = auto()
+  E0002 = auto()
+  SUCCESS = auto()
 
 def get_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("url", help="target url")
-    parser.add_argument("selector", help="selector")
-    parser.add_argument("-a", "--attribute", help="target attribute")
-    parser.add_argument("-c", "--content", help="content", action="store_true")
-    return parser
+  parser = argparse.ArgumentParser()
+  parser.add_argument("url", help="target url")
+  parser.add_argument("selector", help="selector")
+  parser.add_argument("-a", "--attribute", help="target attribute")
+  parser.add_argument("-c", "--content", help="content", action="store_true")
+  return parser
+
+def validate_args(args):
+  if (args.attribute is None and args.content is False):
+    return (ResultCode.E0001, "Error: both attribute and content is not specified")
+  if (args.attribute is not None and args.content is True):
+    return (ResultCode.E0002, "Error: both attribute and content is specified")
+  return (ResultCode.SUCCESS, None)
--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from playwright.sync_api import sync_playwright
-from lib.arg import get_parser
+from lib.arg import get_parser, validate_args, ResultCode
 from lib.get import exec as get
 
-with sync_playwright() as playwright:
+def main():
+  with sync_playwright() as playwright:
     parser = get_parser()
     args = parser.parse_args()
+    (code, message) = validate_args(args)
+    if (code is not ResultCode.SUCCESS):
+      print(message)
+      return
     value = get(playwright, args)
     print(value)
+
+if __name__ == '__main__':
+  main()
