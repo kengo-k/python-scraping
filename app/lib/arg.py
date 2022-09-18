19d8a3aafcda27772c8a18e3ada77cf3fbcba5a9
kengo-k <kurobanekengo@gmail.com>
2022-09-18 19:12:49 +0900

    selectorオプションを必須引数に変更

--- app/lib/arg.py
+++ app/lib/arg.py
@@ -3,7 +3,7 @@ import argparse
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("url", help="target url")
-    parser.add_argument("-s", "--selector", help="selector")
+    parser.add_argument("selector", help="selector")
     parser.add_argument("-a", "--attribute", help="target attribute")
     parser.add_argument("-c", "--content", help="content", action="store_true")
     return parser
--- app/tests/test_arg.py
+++ app/tests/test_arg.py
@@ -1,33 +1,34 @@
 import lib.arg as arg
 import unittest
 
-url = "http://foo.com"
+url = 'http://foo.com'
+selector = 'div'
 
 class TestArg(unittest.TestCase):
 
   def test_attribute(self):
     parser = arg.get_parser()
-    args = [url, '--attribute', 'href']
+    args = [url, selector, '--attribute', 'href']
     result = parser.parse_args(args)
     self.assertEqual(result.attribute, 'href')
-    self.assertIsNone(result.selector)
+    self.assertEqual(result.selector, selector)
     self.assertFalse(result.content)
     self.assertEqual(result.url, url)
 
   def test_attribute2(self):
     parser = arg.get_parser()
-    args = [url, '--attribute=href']
+    args = [url, selector, '--attribute=href']
     result = parser.parse_args(args)
     self.assertEqual(result.attribute, 'href')
 
   def test_attribute_short(self):
     parser = arg.get_parser()
-    args = [url, '-a', 'href']
+    args = [url,  selector, '-a', 'href']
     result = parser.parse_args(args)
     self.assertEqual(result.attribute, 'href')
 
   def test_attribute_short2(self):
     parser = arg.get_parser()
-    args = [url, '-a=href']
+    args = [url,  selector, '-a=href']
     result = parser.parse_args(args)
     self.assertEqual(result.attribute, 'href')
