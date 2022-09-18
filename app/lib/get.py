7eaf14feb8e90e43839f62cd570fe064b5664891
kengo-k <kurobanekengo@gmail.com>
2022-09-18 19:29:37 +0900

    ハードコードされていた取得項目をコマンドライン引数の値を使うように修正

--- app/lib/get.py
+++ app/lib/get.py
@@ -2,24 +2,18 @@ import urllib.request
 from urllib.parse import urlparse
 from playwright.sync_api import sync_playwright
 
-def run(playwright, page_url):
+def exec(playwright, args):
 
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
 
     page = context.new_page()
-    page.goto(page_url)
+    page.goto(args.url)
 
-    raw_button_loc = page.locator('text=RAW')
-    raw_link = raw_button_loc.get_attribute('href')
+    raw_button_loc = page.locator(args.selector)
+    raw_link = raw_button_loc.get_attribute(args.attribute)
 
-    parsed_url = urlparse(page_url)
-    scheme = parsed_url.scheme
-    domain = parsed_url.netloc
-    if (raw_link[0] == '/'):
-        print(f'{scheme}://{domain}{raw_link}')
-    else:
-        print(raw_link)
+    print(raw_link)
 
     context.close()
     browser.close()
--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from playwright.sync_api import sync_playwright
-import lib.arg as arg
-import lib.get as get
+from lib.arg import get_parser
+from lib.get import exec as get
 
 with sync_playwright() as playwright:
-    parser = arg.get_parser()
-    parser.parse_args()
-    #page_url = "http://gitbucket.mynet/private/ssh/blob/master/install.sh"
-    #get.run(playwright, page_url)
+    parser = get_parser()
+    args = parser.parse_args()
+    get(playwright, args)
