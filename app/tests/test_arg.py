7b6834b60cb556dcc77aae1d857656233fd04b87
kengo-k <kurobanekengo@gmail.com>
2022-09-18 21:40:42 +0900

    コマンドラインオプションのバリデーションテストを追加

--- app/tests/test_arg.py
+++ app/tests/test_arg.py
@@ -1,4 +1,4 @@
-import lib.arg as arg
+from lib.arg import get_parser, validate_args, ResultCode
 import unittest
 
 url = 'http://foo.com'
@@ -6,8 +6,35 @@ selector = 'div'
 
 class TestArg(unittest.TestCase):
 
+  def test_valid(self):
+    parser = get_parser()
+    args = parser.parse_args([url, selector, '--a', 'href'])
+    (code, message) = validate_args(args)
+    self.assertEqual(code, ResultCode.SUCCESS)
+    self.assertIsNone(message)
+
+  def test_valid2(self):
+    parser = get_parser()
+    args = parser.parse_args([url, selector, '--c'])
+    (code, message) = validate_args(args)
+    self.assertEqual(code, ResultCode.SUCCESS)
+
+  def test_invalid(self):
+    parser = get_parser()
+    args = parser.parse_args([url, selector])
+    (code, message) = validate_args(args)
+    self.assertEqual(code, ResultCode.E0001)
+    self.assertIsNotNone(message)
+
+  def test_invalid2(self):
+    parser = get_parser()
+    args = parser.parse_args([url, selector, '--a=href','--c'])
+    (code, message) = validate_args(args)
+    self.assertEqual(code, ResultCode.E0002)
+    self.assertIsNotNone(message)
+
   def test_attribute(self):
-    parser = arg.get_parser()
+    parser = get_parser()
     args = [url, selector, '--attribute', 'href']
     result = parser.parse_args(args)
     self.assertEqual(result.attribute, 'href')
@@ -16,19 +43,19 @@ class TestArg(unittest.TestCase):
     self.assertEqual(result.url, url)
 
   def test_attribute2(self):
-    parser = arg.get_parser()
+    parser = get_parser()
     args = [url, selector, '--attribute=href']
     result = parser.parse_args(args)
     self.assertEqual(result.attribute, 'href')
 
   def test_attribute_short(self):
-    parser = arg.get_parser()
+    parser = get_parser()
     args = [url,  selector, '-a', 'href']
     result = parser.parse_args(args)
     self.assertEqual(result.attribute, 'href')
 
   def test_attribute_short2(self):
-    parser = arg.get_parser()
+    parser = get_parser()
     args = [url,  selector, '-a=href']
     result = parser.parse_args(args)
     self.assertEqual(result.attribute, 'href')
